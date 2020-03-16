c8af30d0c1391dca4376854c79d61104643cdeef
huangyy <123456>
2020-03-16 09:39:09 +0800

    20200316 前端框架

---
+++ src/static/js/fileexport.js
@@ -0,0 +1,3 @@
+import saveAs from 'file-saver'
+window.saveAs = saveAs;
+export default saveAs;
---
+++ src/views/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="com">
+        <p class="com-item">首页</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .com {
+        font-size: px2rem(12);
+        width: px2rem(1920);
+        height: 20%;
+        color: $red;
+        background-color: #222222;
+        @include flex();
+
+        .com-item {
+            padding: px2rem(12);
+            color: red;
+        }
+    }
+</style>
